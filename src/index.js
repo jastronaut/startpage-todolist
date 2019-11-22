4454df8f5399230f0bd6cdb6eefda3026aee8967
Jas Delgado <jas@jasdelgado.com>
2019-11-22 12:47:34 -0800

    new and improved, now with DuckDuckGo and website shortcuts

--- src/index.js
+++ src/index.js
@@ -4,79 +4,149 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './dropdown.css';
 import * as serviceWorker from './serviceWorker';
-import { BookOpen, Code, Youtube, Cpu, Book } from 'react-feather';
-import request from 'request';
-
-// function Tabs() {
-// 	return (
-// 		<section>
-// 			<a href='#'>time</a>
-// 			<a href='#'>todo</a>
-// 		</section>
-// 	);
-// }
+import { BookOpen, Code, Book } from 'react-feather';
+import { REDDIT_USERNAME } from './secret.js';
 
 function Icon() {
 	return (
 		<div className='icons'>
-			<a className='ico' href='https://developer.mozilla.org/en-US/docs/Web/CSS/Reference#Keyword_index' alt='css reference'><Code /></a>
-			<a className='ico' href='https://news.ycombinator.com' alt='hacker news'><BookOpen /></a>
-			<a className='ico' href='https://0x00sec.org' alt='0x00sec'><Cpu /></a>
-			<a className='ico' href='http://piazza.com/' alt='piazza'><Book /></a>
-			<a className='ico' href='https://www.youtube.com/feed/subscriptions'><Youtube /></a>
+			<a
+				className='ico'
+				href='https://developer.mozilla.org/en-US/docs/Web/CSS/Reference#Keyword_index'
+				alt='css reference'
+			>
+				<Code />
+			</a>
+			<a
+				className='ico'
+				href='https://news.ycombinator.com'
+				alt='hacker news'
+			>
+				<BookOpen />
+			</a>
+			<a className='ico' href='http://piazza.com/' alt='piazza'>
+				<Book />
+			</a>
 		</div>
 	);
 }
 
 function Todo(props) {
-	return(
-		<div className={props.cname} onClick={props.onClick}>{props.value}</div>
+	return (
+		<div className={props.cname} onClick={props.onClick}>
+			{props.value}
+		</div>
 	);
 }
 
 class TodoArea extends React.Component {
 	render() {
-		const todoList = this.props.todoTodos.map((val, index) => (<Todo cname='td td-todo' key={val} value={val} onClick={() => this.props.onClick(val)} />));
-		const doneList = this.props.todosDone.map((val, index) => (<Todo cname='td td-done' key={val} value={val} onClick={() => this.props.onClick(val)} />));
+		const todoList = this.props.todoTodos.map((val, index) => (
+			<Todo
+				cname='td td-todo'
+				key={val}
+				value={val}
+				onClick={() => this.props.onClick(val)}
+			/>
+		));
+		const doneList = this.props.todosDone.map((val, index) => (
+			<Todo
+				cname='td td-done'
+				key={val}
+				value={val}
+				onClick={() => this.props.onClick(val)}
+			/>
+		));
 		return (
 			<section className='todos'>
 				{todoList}
 				{doneList}
 			</section>
-			
 		);
 	}
 }
 
-function TimeArea() {
-	let d = new Date();
-	// creating my own time format so i have control over text colors
-	return (
-	<section>
-		<h1 className='clock'>{d.getHours() === 12 ? 12 : d.getHours() % 12}:{ d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes()} <span className='ampm'>{d.getHours() >= 12 ? 'PM' : 'AM'}</span></h1>
-		<p className="date-str">{d.toDateString()}</p>
-		<p className='greeting'>{d.getHours() >= 12 ? (d.getHours() > 16 ? 'Good evening.' : 'Good afternoon') : 'Good morning.'}</p>
-	</section>
-	);
+class TimeArea extends React.Component {
+	constructor(props) {
+		super(props);
+		const { hours, minutes, amPM, greeting } = this.cr8D8();
+		this.state = {
+			hours: hours,
+			minutes: minutes,
+			amPM: amPM,
+			greeting: greeting,
+		};
+
+		const d = new Date();
+		this.dateStr = d.toDateString();
+
+		this.upd8();
+	}
+
+	cr8D8() {
+		const d = new Date();
+		const baseHours = d.getHours();
+		const mins =
+			d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
+		const hours = baseHours === 12 ? 12 : baseHours % 12;
+		const amPM = baseHours >= 12 ? 'PM' : 'AM';
+		const greeting =
+			baseHours >= 12
+				? baseHours > 16
+					? 'Good evening.'
+					: 'Good afternoon'
+				: 'Good morning.';
+		return {
+			hours: hours,
+			minutes: mins,
+			amPM: amPM,
+			greeting: greeting,
+		};
+	}
+
+	upd8() {
+		setInterval(() => {
+			const { hours, minutes, amPM, greeting } = this.cr8D8();
+
+			this.setState({
+				minutes: minutes,
+				hours: hours,
+				amPM: amPM,
+				greeting: greeting,
+			});
+		}, 6000);
+	}
+
+	render() {
+		return (
+			<section>
+				<h1 className='clock'>
+					{this.state.hours}:{this.state.minutes}{' '}
+					<span className='ampm'>{this.state.amPM}</span>
+				</h1>
+				<p className='date-str'>{this.dateStr}</p>
+				<p className='greeting'>{this.state.greeting}</p>
+			</section>
+		);
+	}
 }
 
 function Suggestion(props) {
-	return (
-		<p onClick={props.onClick}>{props.value}</p>
-	);
+	return <p onClick={props.onClick}>{props.value}</p>;
 }
-
 
 class Dropdown extends React.Component {
 	render() {
 		// render list of dropdown items thru props
-		const suggestions = this.props.suggestions.map((val, index) => (<Suggestion key={val} value={val} onClick={() => this.props.onClick(val)} />));
+		const suggestions = this.props.suggestions.map((val, index) => (
+			<Suggestion
+				key={val}
+				value={val}
+				onClick={() => this.props.onClick(val)}
+			/>
+		));
 
-		return (
-				<div className='dd-content'>
-				{suggestions}
-				</div>
-		);
+		return <div className='dd-content'>{suggestions}</div>;
 	}
 }
 
@@ -99,7 +169,7 @@ class StartPage extends React.Component {
 		let st = {
 			newTodo: '',
 			search: '',
-			suggestions: []
+			suggestions: [],
 		};
 		if (this.storage.getItem('todosDone')) {
 			st.todosDone = this.storage.getItem('todosDone').split(',');
@@ -119,23 +189,25 @@ class StartPage extends React.Component {
 		const todosDone = this.state.todosDone;
 		const todoTodos = this.state.todoTodos;
 
-		if (todosDone.indexOf(i) >= 0) { // if this task is already done, mark as undone
+		if (todosDone.indexOf(i) >= 0) {
+			// if this task is already done, mark as undone
 			todoTodos.push(i);
 			todosDone.splice(todosDone.indexOf(i), 1);
-		} else { // if this task has not been done, mark it as done
+		} else {
+			// if this task has not been done, mark it as done
 			todoTodos.splice(todoTodos.indexOf(i), 1);
-			todosDone.push(i);	
+			todosDone.push(i);
 		}
 		this.storage.setItem('todoTodos', todoTodos);
 		this.storage.setItem('todosDone', todosDone);
 		this.setState({
 			todoTodos: todoTodos,
-			todosDone: todosDone
+			todosDone: todosDone,
 		});
 	}
 
 	handleInputChange(event) {
-		this.setState({newTodo: event.target.value});
+		this.setState({ newTodo: event.target.value });
 	}
 
 	handleSubmit(event) {
@@ -148,52 +220,66 @@ class StartPage extends React.Component {
 				todoTodos: todoTodos,
 			});
 		}
-		this.setState({newTodo: ''});
+		this.setState({ newTodo: '' });
 		event.preventDefault();
 	}
 
 	handleClearDone() {
 		this.setState({
-			todosDone: []
+			todosDone: [],
 		});
 		this.storage.setItem('todosDone', '');
 	}
 
 	handleClearAll() {
 		this.setState({
-			todosDone: []
+			todosDone: [],
 		});
 		this.storage.setItem('todosDone', '');
 		this.setState({
-			todoTodos: []
+			todoTodos: [],
 		});
 		this.storage.setItem('todoTodos', '');
 	}
+	async getSuggestions(query) {
+		// http://api.bing.com/osjson.aspx?query=
+		try {
+			const resp = await fetch(
+				'https://cors-anywhere.herokuapp.com/http://api.bing.com/osjson.aspx?query=' +
+					encodeURIComponent(query),
+				{
+					method: 'GET',
+				}
+			);
+			const autocompleteSuggestions = await resp.json();
+			this.setState({
+				suggestions: autocompleteSuggestions[1].slice(0, 5),
+			});
+		} catch (err) {
+			console.log('oops');
+		}
+	}
 
 	handleSearchInput(event) {
+		const query = event.target.value;
+
 		this.setState({
-			search: event.target.value
+			search: query,
 		});
 
-		if (this.state.search === '') {
-			this.setState({suggestions: []});
+		if (query === '') {
+			this.setState({ suggestions: [] });
 			return;
 		}
 
-		// http://api.bing.com/osjson.aspx?query=
-
-		request('http://api.bing.com/osjson.aspx?query=' + encodeURIComponent(event.target.value), {json: true}, (err, res, body) => {
-			if (err) {
-				return;
-			}
-			this.setState({suggestions: body[1].slice(0, 5)});
-		});
-
+		this.getSuggestions(query);
 	}
 
 	searchRedirect(search) {
 		// i use bing for microsoft rewards points :~)
-		window.location.replace("https://www.bing.com/search?q=" + encodeURIComponent(search) + "&adlt=strict");
+		window.location.replace(
+			'https://www.duckduckgo.com/?q=' + encodeURIComponent(search)
+		);
 	}
 
 	onClickSuggestion(i) {
@@ -202,23 +288,65 @@ class StartPage extends React.Component {
 
 	handleSearch(event) {
 		const search = this.state.search.trim();
+
+		switch (search) {
+			case 'mess':
+				window.location.replace('https://www.messenger.com/');
+				event.preventDefault();
+				return;
+			case 'redd':
+				window.location.replace('https://www.reddit.com/');
+				event.preventDefault();
+				return;
+			case 'paper':
+				window.location.replace('https://paper.dropbox.com/');
+				event.preventDefault();
+				return;
+			case 'news':
+				window.location.replace('https://news.ycombinator.com/news');
+				event.preventDefault();
+				return;
+			case 'yt':
+				window.location.replace(
+					'https://invidio.us/feed/subscriptions'
+				);
+				event.preventDefault();
+				return;
+			case 'feed':
+				window.location.replace(
+					'https://www.reddit.com/user/' +
+						REDDIT_USERNAME +
+						'/m/lyfe/'
+				);
+				event.preventDefault();
+				return;
+			case 'css':
+				window.location.replace(
+					'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference#Keyword_index'
+				);
+				event.preventDefault();
+				return;
+			default:
+				break;
+		}
 		// look for "valid" url and direct to it if entered, otherwise search for query
 
 		// valid url needs: http/s protocol, .com/net/org/edu, no whitespace
-		var websiteQuery = RegExp(/http(s{0,1}):\/\/(\S)+\.(com|net|org|edu)\S{0,}$/);
+		var websiteQuery = RegExp(
+			/http(s{0,1}):\/\/(\S)+\.(com|net|org|edu)\S{0,}$/
+		);
 		if (websiteQuery.test(search)) {
 			window.location.replace(search);
 		} else {
 			this.searchRedirect(search);
-			
 		}
 		event.preventDefault();
 	}
 
 	handleKeyPress(event) {
 		// clear search bar when escape is pressed
-		if (event.keyCode === 27) {
-			this.setState({search: '', suggestions: []});
+		if (event.keyCode === 27 || event.target.value === '') {
+			this.setState({ search: '', suggestions: [] });
 		}
 	}
 
@@ -226,30 +354,55 @@ class StartPage extends React.Component {
 		return (
 			<section>
 				<TimeArea />
-				
+
 				{/* search bar area */}
 				<form onSubmit={this.handleSearch}>
 					<div className='dropdown'>
-					<input autoCorrect="off" className='search' autoFocus type='text' placeholder='search' value={this.state.search} onChange={this.handleSearchInput} onKeyDown={(event) => this.handleKeyPress(event)} />
-					<Dropdown suggestions={this.state.suggestions} onClick={i => this.onClickSuggestion(i)} />
+						<input
+							autoCorrect='off'
+							className='search'
+							autoFocus
+							type='text'
+							placeholder='search'
+							value={this.state.search}
+							onChange={this.handleSearchInput}
+							onKeyDown={event => this.handleKeyPress(event)}
+						/>
+						<Dropdown
+							suggestions={this.state.suggestions}
+							onClick={i => this.onClickSuggestion(i)}
+						/>
 					</div>
 				</form>
-				
+
 				{/* todolist input area */}
 				<form onSubmit={this.handleSubmit}>
-					<input autoCorrect="off" type='text' placeholder='todo' value={this.state.newTodo} onChange={this.handleInputChange} />
+					<input
+						autoCorrect='off'
+						type='text'
+						placeholder='todo'
+						value={this.state.newTodo}
+						onChange={this.handleInputChange}
+					/>
 				</form>
 
 				{/* actual todolist */}
-				<TodoArea todoTodos={this.state.todoTodos} todosDone={this.state.todosDone} onClick={i => this.handleClick(i)} />
-				<div className='clear-todos' onClick={this.handleClearDone}>clear done</div>
-				<div className='clear-todos' onClick={this.handleClearAll}>clear all</div>
+				<TodoArea
+					todoTodos={this.state.todoTodos}
+					todosDone={this.state.todosDone}
+					onClick={i => this.handleClick(i)}
+				/>
+				<div className='clear-todos' onClick={this.handleClearDone}>
+					clear done
+				</div>
+				<div className='clear-todos' onClick={this.handleClearAll}>
+					clear all
+				</div>
 				<Icon />
 			</section>
 		);
 	}
 }
-
 
 ReactDOM.render(<StartPage />, document.getElementById('time'));
 
